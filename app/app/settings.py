81c9e40a66974700cba07b51066b2520bbc5a7ea
Manish Malasane <mmalasane6@gmail.com>
2023-04-23 13:10:35 +0530

    Django project settings package

--- app/app/settings.py
+++ app/app/settings.py
@@ -125,3 +125,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# when we customize default user model after that we have to set our custom user model to default user model
+# for that we have to do this
+AUTH_USER_MODEL = "core.user"
+
