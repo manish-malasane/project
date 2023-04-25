ecaefd20dd379ea08134b72d437a320bec574a3a
Manish Malasane <mmalasane6@gmail.com>
2023-04-25 19:06:07 +0530

    ModelSerializer and plane serializer for token generation, authenticate mehtod

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
+
 # from app.core.models import User
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, authenticate
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -25,7 +26,8 @@ class UserSerializer(serializers.ModelSerializer):
         # automatically provide extra validation on ``fields`` directive when try to hit post request from browser
         extra_kwargs = {
             "password": {"write_only": True, "min_length": 5},
-            "email": {"min_length": 5}}
+            "email": {"min_length": 5},
+        }
 
     def create(self, validated_data):
         """
@@ -43,8 +45,50 @@ class UserSerializer(serializers.ModelSerializer):
         # Reusing parent class method
         user = super().update(instance, validated_data)
 
-        if password: # If password then update from here otherwise update everything from parent class
+        if (
+            password
+        ):  # If password then update from here otherwise update everything from parent class
             user.set_password(password)
             user.save()
 
         return user
+
+
+class AuthTokenSerializer(serializers.Serializer):
+    """
+    Serializer for user-authentication token
+    """
+
+    email = serializers.EmailField()
+
+    # trim_whitespace is a directive to trim whitespaces default it is True but here we set False
+    # To capture whitespaces in password
+    password = serializers.CharField(trim_whitespace=False)
+
+    def validate(self, data):
+        """
+        Validate and authenticate the user
+        """
+
+        # data is a dict
+        email = data.get("email")
+
+        if email.isupper():  # if email domain name is in upper format will get error
+            raise serializers.ValidationError("Email is not as per standards")
+
+        password = data.get("password")
+
+        # authenticate method takes request which comes from `self.context.get("request")`
+        # and also takes credentials
+        user = authenticate(
+            self.context.get("request"), username=email, password=password
+        )
+
+        if not user:
+            raise serializers.ValidationError(
+                "Unable to authenticate user with given credentials",
+                code="authorization",
+            )  # code is HTTP code not more than that
+
+        data["user"] = user
+        return data
