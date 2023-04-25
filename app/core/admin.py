963a514e7f4f4f8b3351cba6e3796faf5cab7f31
Manish Malasane <mmalasane6@gmail.com>
2023-04-25 10:52:41 +0530

    Customization of django admin site template

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,6 +1,50 @@
 from django.contrib import admin  # noqa
 
 # Register your models here.
-from .models import User
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-admin.site.register(User)  # registering the custom user model
+# TODO
+# refer
+# https://docs.djangoproject.com/en/4.2/ref/contrib/admin/actions/#adding-actions-to-the-modeladmin
+
+#  TODO - refer -
+#  https://docs.djangoproject.com/en/4.1/topics/i18n/translation/
+# https://docs.djangoproject.com/en/4.1/topics/i18n/translation/#internationalization-in-python-code
+#  Required to globalize our project
+#  Required for translation
+
+from django.utils.translation import gettext_lazy as _
+
+
+class UserAdmin(BaseUserAdmin):
+    ordering = ("id",)
+    list_display = ("email", "name")
+
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (_("Personal Information"), {"fields": ("name",)}),
+        (_("Permissions"), {"fields": ("is_active", "is_staff", "is_superuser")}),
+        (_("Important Dates"), {"fields": ("last_login",)}),
+    )
+
+    readonly_fields = ["last_login"]
+
+    add_fieldsets = (
+        None,
+        {
+            "classes": ("wide",),
+            "fields": (
+                "email",
+                "password1",
+                "password2",
+                "name",
+                "is_active",
+                "is_staff",
+                "is_superuser",
+            ),
+        },
+    )
+
+
+admin.site.register(get_user_model(), UserAdmin)  # registering the custom user model
