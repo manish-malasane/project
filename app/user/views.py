2418f1639f5c8b7e5a2b7d8d6883cdbe9008b541
Manish Malasane <mmalasane6@gmail.com>
2023-04-26 13:22:30 +0530

    DRF generics views for CRUD operation & ObtainAuthToken & permissions and authentication

--- app/user/views.py
+++ app/user/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
-from rest_framework.generics import CreateAPIView
-from .serializers import UserSerializer, AuthTokenSerializer
-from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.generics import (
+    CreateAPIView,
+    RetrieveUpdateAPIView,
+)  # DRF Generics for CRUD operations
+from .serializers import (
+    UserSerializer,
+    AuthTokenSerializer,
+)  # Custom serializers (ModelSerializers)
+from rest_framework.authtoken.views import ObtainAuthToken  # For TokenAuthentication
+from rest_framework import (
+    authentication,
+    permissions,
+)  # This is authentication for any specific view
 
 # Create your views here.
 
@@ -26,3 +36,38 @@ class CreateTokenView(ObtainAuthToken):
 
     # Over-riding serializer_class variable with our custom serializer
     serializer_class = AuthTokenSerializer
+
+
+class ManageUserView(RetrieveUpdateAPIView):
+    """
+    This view helps us to perform following http methods
+    - GET
+    - PUT
+    - PATCH
+
+    TODO
+    refer -
+    https://www.django-rest-framework.org/api-guide/generic-views/#retrieveupdateapiview
+    """
+
+    # Configuring which serializer we need for that over-riding this directive
+    serializer_class = UserSerializer
+
+    # In our project we use TokenAuthentication
+    # Here we are over-riding `authentication_classes` directive for which type of authentication we need
+    # TODO - Refer
+    # https://www.django-rest-framework.org/api-guide/authentication/#tokenauthentication
+    authentication_classes = [authentication.TokenAuthentication]
+
+    # Over-riding `permission_classes` directive for permission to authenticate the given
+    # TODO - refer
+    # https://www.django-rest-framework.org/api-guide/permissions/#setting-the-permission-policy
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        """
+        Retrieve user and return authenticated user information
+        TODO - refer
+        https://www.django-rest-framework.org/api-guide/permissions/#object-level-permissions
+        """
+        return self.request.user  # helps to retrieve user who sends the request
