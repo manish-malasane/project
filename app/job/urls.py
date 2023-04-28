d09727ef6157e1d71a9f06e0bc76fe62b3855fae
Manish Malasane <mmalasane6@gmail.com>
2023-04-28 17:34:29 +0530

    Urls mapping including all applications urls (endpoint)

--- app/job/urls.py
+++ app/job/urls.py
@@ -1,1 +1,6 @@
-url_patterns = []
+from django.urls import path
+from app1.job import views
+
+url_patterns = [
+    path("job-title/", views.JobTitleViewSet.as_view({"get": "list"}), name="Titles")
+]
