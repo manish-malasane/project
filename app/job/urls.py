35be92a541dae3d575d5b0c930a75948781531de
Manish Malasane <mmalasane6@gmail.com>
2023-04-28 17:38:41 +0530

    Urls mapping/binding of views

--- app/job/urls.py
+++ app/job/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from app1.job import views
+from job import views
 
-url_patterns = [
-    path("job-title/", views.JobTitleViewSet.as_view({"get": "list"}), name="Titles")
+urlpatterns = [
+    path("jobtitle/", views.JobTitleViewSet.as_view({"get": "list"}), name="Titles")
 ]
